7f2000a55b4a2984469b35c72c05249d30041c28
quanghoatrinh <quanghoa.trinh@gmail.com>
2024-06-13 14:54:16 +0700

    advance search

--- src/main/java/com/example/t2208a_springboot/controller/ProductController.java
+++ src/main/java/com/example/t2208a_springboot/controller/ProductController.java
@@ -3,8 +3,10 @@ package com.example.t2208a_springboot.controller;
 import com.example.t2208a_springboot.entity.Product;
 import com.example.t2208a_springboot.model.response_model.ProductResponse;
 import com.example.t2208a_springboot.service.ProductService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -25,4 +27,9 @@ public class ProductController {
     // Create
     // Update
     // Delete
+
+    public ResponseEntity<List<Product>>search(){
+
+        return null;
+    }
 }
--- src/main/java/com/example/t2208a_springboot/repository/ProductRepository.java
+++ src/main/java/com/example/t2208a_springboot/repository/ProductRepository.java
@@ -2,8 +2,25 @@ package com.example.t2208a_springboot.repository;
 
 import com.example.t2208a_springboot.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product,Long> {
+    List<Product> findAllByName(String name); // find
+    List<Product> findAllByNameContaining(String name); // search
+    List<Product> findAllByNameContainingOrPrice(String name,Double price);
+    // AdvanceSearch
+    @Query("SELECT p from Product p "+
+            "WHERE (:name is NULL OR p.name LIKE %:name%) "+
+            "AND (:minPrice is NULL OR p.price >= :minPrice) "+
+            "AND (:maxPrice is NULL OR p.price <= :maxPrice) "
+    )
+    List<Product> search(@Param("name") String name,
+                         @Param("minPrice") Double minPrice,
+                         @Param("maxPrice") Double maxPrice
+                         );
 }
